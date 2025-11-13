edd949b3aa9de1aadaf31532f5c9e4f3da658aa0
lucaster <luca.cavagnoli@gmail.com>
2025-11-13 13:32:01 +0100

    links in component field

--- sandbox-generator-ng/src/app/app.ts
+++ sandbox-generator-ng/src/app/app.ts
@@ -8,5 +8,15 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
+
   protected readonly title = signal('sandbox-generator-ng');
+
+  public readonly links = [
+    { title: 'Explore the Docs', link: 'https://angular.dev' },
+    { title: 'Learn with Tutorials', link: 'https://angular.dev/tutorials' },
+    { title: 'Prompt and best practices for AI', link: 'https://angular.dev/ai/develop-with-ai'},
+    { title: 'CLI Docs', link: 'https://angular.dev/tools/cli' },
+    { title: 'Angular Language Service', link: 'https://angular.dev/tools/language-service' },
+    { title: 'Angular DevTools', link: 'https://angular.dev/tools/devtools' },
+  ];
 }
