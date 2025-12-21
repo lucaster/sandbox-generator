ef214b78be383977ddb97f83caea109bce330230
Luca <luca.cavagnoli@gmail.com>
2025-12-21 15:29:00 +0100

    refact

--- sandbox-generator-ng/src/app/pages/hex-draw-biome/hex-draw-biome.ts
+++ sandbox-generator-ng/src/app/pages/hex-draw-biome/hex-draw-biome.ts
@@ -219,7 +219,7 @@ export class HexDrawBiome implements AfterViewInit {
     svgDrawer.drawTextAtPoint(center_19, 19);
   }
 
-  resetSvg(): void {
+  resetPatch(): void {
     const svg = this.svgElement.nativeElement;
     while (svg.firstChild) {
       svg.removeChild(svg.firstChild);
